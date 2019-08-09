a546bf42b79ab74c5cb780671a4c99d9aa80c86e
sectumsempra30 <m.gnatyuk16@gmail.com>
2019-08-09 08:37:53 +0300

    fix

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const hotController = require('./controllers/hotdogcontroller');
 var bodyParser = require('body-parser');
 
+const { PORT } = process.env
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res, next) => res.json({ text: 'hello world!' }))
 app.get('/hotdogs', hotController.all);
 app.post('/hotdogs', hotController.create);
 app.put('/hotdogs/:id', hotController.update);
 app.delete('/hotdogs/:id', hotController.delete);
 
 app.listen(
-    3000,
-    () => console.log("Server is started")
+    PORT,
+    () => console.log(`Application started at :${PORT}`)
 );
